e2e19086ffd5218943f588c8cfce8ee5b6d15352
Maximilian Bärtel <maximilian.baertel@mum.de>
2026-01-23 14:24:42 +0100

    Update Bus

--- MonsterBattleEngine.Core/BattleFlow/BattleEventBus.cs
+++ MonsterBattleEngine.Core/BattleFlow/BattleEventBus.cs
@@ -8,34 +8,86 @@ namespace MonsterBattleEngine.Core.BattleFlow
 {
     public class BattleEventBus : IBattleEventBus
     {
+        private struct HandlerInfo
+        {
+            public int Id;
+            public object Owner;
+            public Delegate Handler;
+            public int Priority;
+            public bool Once;
+        }
+
+        private readonly Dictionary<Type, List<HandlerInfo>> _handlers = new();
+        private int _nextHandlerId = 1;
+
         public void Publish<T>(T evt) where T : IBattleEvent
         {
-            throw new NotImplementedException();
+            var type = typeof(T);
+            if (!_handlers.TryGetValue(type, out var handlers)) return;
+            var orderedHandlers = handlers.OrderByDescending(h => h.Priority).ToList();
+
+            foreach (var h in orderedHandlers)
+            {
+                ((Action<T>)h.Handler)(evt);
+
+                if (h.Once)
+                {
+                    handlers.Remove(h);
+                }
+            }
         }
 
         public int Subscribe<T>(object owner, Action<T> handler, int priority = 0) where T : IBattleEvent
         {
-            throw new NotImplementedException();
+            return AddHandler(owner, handler, priority, once: false);
         }
 
         public void SubscribeOnce<T>(object owner, Action<T> handler, int priority = 0) where T : IBattleEvent
         {
-            throw new NotImplementedException();
+            AddHandler(owner, handler, priority, once: true);
         }
 
         public void Unsubscribe<T>(object owner) where T : IBattleEvent
         {
-            throw new NotImplementedException();
+            var type = typeof(T);
+            if (!_handlers.TryGetValue(type, out var handlers)) return;
+
+            handlers.RemoveAll(h => h.Owner == owner);
         }
 
         public void Unsubscribe(int handlerId)
         {
-            throw new NotImplementedException();
+            foreach (var kvp in _handlers)
+            {
+                kvp.Value.RemoveAll(h => h.Id == handlerId);
+            }
         }
 
         public void UnsubscribeAll(object owner)
         {
-            throw new NotImplementedException();
+            foreach (var kvp in _handlers)
+            {
+                kvp.Value.RemoveAll(h => h.Owner == owner);
+            }
+        }
+
+        private int AddHandler<T>(object owner,Action<T> handler,int priority,bool once = false)
+        {
+            var type = typeof(T);
+            if (!_handlers.ContainsKey(type))
+                _handlers[type] = new List<HandlerInfo>();
+
+            var info = new HandlerInfo
+            {
+                Id = _nextHandlerId++,
+                Owner = owner,
+                Handler = handler,
+                Priority = priority,
+                Once = once
+            };
+
+            _handlers[type].Add(info);
+            return info.Id;
         }
     }
 }
